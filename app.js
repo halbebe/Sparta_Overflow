37d256173635398acaa98cdb502a6877b339f7f2
halbebe <gksmfsf97@naver.com>
2023-12-27 10:04:37 +0900

    죽고싶어요

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import { SERVER_PORT } from './src/constants/app.constant.js';
+import { apiRouter } from './src/routers/index.js';
+import { errorHandler } from './src/middlewares/error-handler.middleware.js';
 
 const app = express();
-const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/api', apiRouter);
+app.use(errorHandler);
 
 app.listen(SERVER_PORT, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${SERVER_PORT}`);
 });
